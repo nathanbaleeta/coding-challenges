e33628d401bb9de3329760dc248c39a8801ceb63
Nathan Baleeta <nbaleeta@gmail.com>
2026-07-12 15:18:01 +0300

    return string after first if else conditional

--- python/strings/4_aardvark_zebra_index.py
+++ python/strings/4_aardvark_zebra_index.py
@@ -4,12 +4,12 @@ Write a function called aardvark, given a string returns 'aardvark' if the strin
 
 def aardvark(string) -> str:
     if string[0] == 'a':
-        return aardvark
+        return 'aardvark'
     else:
         return 'zebra'
     
 
 if __name__ == "__main__": 
     print(aardvark("arg"))
-    print(aardvark("trinket"))
+    print(aardvark("Trinket"))
     
