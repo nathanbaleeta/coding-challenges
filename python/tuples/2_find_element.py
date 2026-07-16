9d048f86d9e5f23f77c41e22826205dacfd48067
Nathan Baleeta <nbaleeta@gmail.com>
2026-07-16 16:21:18 +0300

    add function, tuple_unpacker

--- python/tuples/2_find_element.py
+++ python/tuples/2_find_element.py
@@ -1,5 +1,6 @@
 '''
-Goal: Write a function that takes a tuple and an element. If the element exists in the tuple, return its index. If it doesn't, return -1. Do not let the code throw a ValueError.
+Goal: You are given a tuple containing a user's first name, last name, age, and city. 
+Write a function that unpacks these values and returns a formatted string: "First Last is Age years old and lives in City."
 '''
 
 """
