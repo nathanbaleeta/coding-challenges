228bd53978e8c80d0a68dd256959dacb9e864f7c
Nathan Baleeta <nbaleeta@gmail.com>
2026-07-17 13:10:17 +0300

    modified sum_numbers function to robustly check data types before summing digits

--- python/tuples/4_sum_numbers.py
+++ python/tuples/4_sum_numbers.py
@@ -4,6 +4,9 @@ Goal: Tuples can hold mixed data types. Write a function that takes a tuple and 
 
 def sum_numbers(tup: tuple) -> float:
     # Note: Python treats True as 1, so filter carefully!
+    ######################################
+    # Solution 1:
+    ######################################
     """
     sum = 0
     for i in tup:
@@ -12,12 +15,25 @@ def sum_numbers(tup: tuple) -> float:
     return sum
     """
 
+    ######################################
+    # Solution 2:
+    ######################################
+    """
     sum = 0
     for i in tup:
         if not isinstance(i, (str, bool)):
             sum += i 
     return sum
+    """
 
+    ######################################
+    # Solution 3:
+    ######################################
+    sum = 0
+    for i in tup:
+        if isinstance(i, (int, float)) and not isinstance(i, bool):
+            sum += i 
+    return sum
 
     
 # Test cases
